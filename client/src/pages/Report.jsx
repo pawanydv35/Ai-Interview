6ea586a5b9649dc1e069e18240e14c2e91a3733c
Pawan Yadav <pawanydv3010@gmail.com>
2026-02-21 10:32:40 +0530

    dummy report

---
+++ client/src/pages/Report.jsx
@@ -0,0 +1,13 @@
+function Report() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Performance Report</h1>
+      <p>Total Questions: 10</p>
+      <p>Correct Answers: 7</p>
+      <p>Score: 70%</p>
+      <button>Retake Quiz</button>
+    </div>
+  );
+}
+
+export default Report;
