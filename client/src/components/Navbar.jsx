0c2817e0cabb03c0f18f9af33c9dda37f44cca27
Pawan Yadav <pawanydv3010@gmail.com>
2026-02-21 10:39:20 +0530

    navbar styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,25 +2,19 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={styles.nav}>
-      <h2>AI Interview Pro</h2>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/quiz">Quiz</Link>
-        <Link to="/about">About</Link>
+    <nav style={{ padding: "20px 40px", background: "#0f172a" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h2 style={{ color: "#8b5cf6" }}>AI Interview Pro</h2>
+
+        <div style={{ display: "flex", gap: "20px" }}>
+          <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
+          <Link to="/quiz" style={{ color: "white", textDecoration: "none" }}>Quiz</Link>
+          <Link to="/report" style={{ color: "white", textDecoration: "none" }}>Report</Link>
+          <Link to="/about" style={{ color: "white", textDecoration: "none" }}>About</Link>
+        </div>
       </div>
     </nav>
   );
 }
-
-const styles = {
-  nav: {
-    display: "flex",
-    justifyContent: "space-between",
-    padding: "15px 30px",
-    background: "#111",
-    color: "#fff"
-  }
-};
 
 export default Navbar;
